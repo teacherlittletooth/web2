d92b4a0fea06d54cae1184ef7a32dcb69a9fff69
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-10-25 16:57:29 -0300

    Create data.php

---
+++ aula4/data.php
@@ -0,0 +1,30 @@
+<?php
+
+//Construindo lista associativa
+$games = array(
+    "101" => [
+        "jogo" => "God of War",
+        "ano" => "2005",
+        "genero" => "Ação"
+    ],
+    "102" => [
+        "jogo" => "GTA San Andreas",
+        "ano" => "2004",
+        "genero" => "Criminoso"
+    ],
+    "103" => [
+        "jogo" => "Minecraft",
+        "ano" => "2009",
+        "genero" => "Sand Box"
+    ],
+    "104" => [
+        "jogo" => "Roblox",
+        "ano" => "2006",
+        "genero" => "Aventura"
+    ],
+    "105" => [
+        "jogo" => "Winning Eleven",
+        "ano" => "2000",
+        "genero" => "Esporte"
+    ]
+);
