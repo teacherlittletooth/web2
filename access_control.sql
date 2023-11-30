5b616e2729565ce4547366ee742baf6657ff0bed
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-11-30 20:50:01 -0300

    Update access_control.sql

--- access_control.sql
+++ access_control.sql
@@ -32,7 +32,7 @@ USE access_control;
 --
 
 CREATE TABLE `users` (
-  `user_cod` int(11) NOT NULL AUTO_INCREMENT,
+  `user_cod` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `user_name` varchar(20) NOT NULL,
   `user_pass` text NOT NULL,
   `user_photo` text DEFAULT NULL,
@@ -54,19 +54,6 @@ CREATE TABLE `users` (
   `user_new_pass` text DEFAULT NULL,
   `user_status` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `users`
---
-
-INSERT INTO `users` (`user_cod`, `user_name`, `user_pass`, `user_photo`, `user_birth`, `user_first_name`, `user_last_name`, `user_desc`, `user_cep`, `user_address`, `user_number`, `user_complement`, `user_neighborhood`, `user_city`, `user_state`, `user_sex`, `user_phone`, `user_email`, `user_notify`, `user_new_pass`, `user_status`) VALUES
-(1, 'picapau', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', NULL, '1999-05-20', 'Pica', 'Pau', 'Passarinho muito chato.', '12345678', 'Aldeia do Zeca Urubú', '321', 'Nenhum', 'Walter Lantz', 'Washington', NULL, 'male', '999999999', 'pica@pau.com', 1, NULL, 1),
-(2, 'picolino', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '', '2010-10-10', 'Pico', 'Lino', 'Pinguim sem graça que aparece nos desenhos do pica-pau.', '123456', 'Polo Norte', '123', 'Iglú', 'Gelado', 'Vila Elza', 'SE', 'male', '55-555555555', 'picolino@picapau.com', 1, '', NULL),
-(3, 'george', 'fc7a734dba518f032608dfeb04f4eeb79f025aa7', '', '2003-02-05', 'Georgino', 'Alvarenga', 'Uma pessoa muito estimada pelos seus.', '7891011', '', '', '', '', 'Alvorada', 'RS', 'male', '', 'george@peppapig.com', 1, '', NULL);
-
---
--- Índices para tabelas despejadas
---
 
 --
 -- Índices para tabela `users`
